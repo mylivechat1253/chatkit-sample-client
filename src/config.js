c879041d2a5834e8c596df3bfc2be5eeb42ea3a6
mylivechat1253 <62119833+mylivechat1253@users.noreply.github.com>
2020-03-13 13:55:19 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const auth0Domain = 'chatkit-demo'
+const auth0Domain = 'dev-5nups0wd.auth0.com'
 const auth0ClientID = '2j5b1b2L8Fn9BIvDbWDWijzAPaiQKksB'
 const auth0RedirectUri = 'https://chatkit-demo.netlify.com/'
 // const auth0RedirectUri = 'http://localhost:3000' /* Testing locally */
