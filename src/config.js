96c534a32abbdc1aae977b6c16dd32e515edf288
mylivechat1253 <62119833+mylivechat1253@users.noreply.github.com>
2020-03-13 13:53:21 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
-const auth0Domain = 'pusher-chatkit-demo.auth0.com'
-const auth0ClientID = 'VF0kHM9szMj0aQGEfqafrdAKVwRb2a3Q'
-const auth0RedirectUri = 'https://chatkit-sample.netlify.com/'
+const auth0Domain = 'chatkit-demo'
+const auth0ClientID = '2j5b1b2L8Fn9BIvDbWDWijzAPaiQKksB'
+const auth0RedirectUri = 'https://chatkit-demo.netlify.com/'
 // const auth0RedirectUri = 'http://localhost:3000' /* Testing locally */
 
 const chatkitTokenProviderEndpoint = '.netlify/functions/token'
 
-const chatkitInstanceLocator = 'v1:us1:7954c374-f491-4c08-b71e-5abfc0a3dc89'
+const chatkitInstanceLocator = 'v1:us1:d34fed07-c293-4ede-8fa1-e726951533a9'
 
 export { auth0Domain, auth0ClientID, auth0RedirectUri, chatkitTokenProviderEndpoint, chatkitInstanceLocator }
